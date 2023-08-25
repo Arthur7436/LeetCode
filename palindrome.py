c02c7d61577c098c0d18cd707c8ca2dbf19eca63
arthur <arthur.thai479@gmail.com>
2023-08-25 17:01:48 +1000

    update

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,13 @@
-userInput = input('Please ener a number to see if it is a palindrome: ')
-reversedString = userInput[::-1]
+while True:
+    userInput = input('Please ener a number to see if it is a palindrome: ')
+    reversedString = userInput[::-1]
 
 
-if userInput == reversedString:
-    print("It is a palindrome!")
-else:
-    print("It is not a palindrome!")
+    if userInput == reversedString:
+        print("It is a palindrome!")
+        print("The program has closed")
+        break
+    else:
+        print("It is not a palindrome!")
+       
+        
