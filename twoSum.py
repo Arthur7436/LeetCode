3bb0e21e61f75bc31da03c1bb19a134db88396ed
Arthur Thai <athai@challenger.com.au>
2023-08-31 17:41:31 +1000

    update

--- twoSum.py
+++ twoSum.py
@@ -1,4 +1,10 @@
 nums = [2,7,11,15]
 target = 9
 
-//comment
+# iterate the first number with the other numbers
+for i in nums:
+    for j in nums:
+        print(i)
+# if the sum = target then print the two numbers in a list
+        
+
