7fcc6922e6d29ff5f8d573ff3705e2bc3a1755d9
Arthur Thai <athai@challenger.com.au>
2023-08-31 18:13:32 +1000

    update

--- twoSum.py
+++ twoSum.py
@@ -2,9 +2,11 @@ nums = [2,7,11,15]
 target = 9
 
 # iterate the first number with the other numbers
-for i in nums:
-    for j in nums:
-        print(i)
+total = print(nums[0] + nums[1])
+if total == 9:
+    print('You got it!')
+
 # if the sum = target then print the two numbers in a list
+# if not then move onto the next number and repeat above step
         
 
